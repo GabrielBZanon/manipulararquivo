d98a3e39e01b7b1dc14b3ee30535accacce3986d
GabrielBZanon <161066685+GabrielBZanon@users.noreply.github.com>
2024-06-03 16:10:39 -0300

    Add files via upload

---
+++ ex5LOP/desencriptado.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CHAVE 3
+
+int main() {
+    FILE *arquivo_entrada, *arquivo_saida;
+    char caractere;
+
+    arquivo_entrada = fopen("teste5.txt", "r");
+    if (arquivo_entrada == NULL) {
+        printf("Erro ao abrir o arquivo de entrada.\n");
+        return 1;
+    }
+
+    arquivo_saida = fopen("teste5saida.txt", "w");
+    if (arquivo_saida == NULL) {
+        printf("Erro ao criar o arquivo de saída.\n");
+        fclose(arquivo_entrada);
+        return 1;
+    }
+
+    while ((caractere = fgetc(arquivo_entrada))!= EOF) {
+        caractere -= CHAVE;
+        fputc(caractere, arquivo_saida);
+    }
+
+    fclose(arquivo_entrada);
+    fclose(arquivo_saida);
+
+    printf("Arquivo desencriptado com sucesso.\n");
+
+    return 0;
+}
---
+++ exercício2LOP/removedor.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//programa alô mundo
+int main() {
+    printf("Alo mundo!\n");
+    return 0;
+}
